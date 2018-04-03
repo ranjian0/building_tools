59e62029444c2cb93aeef11c5c0ed11194c86f95
ranjian0 <karanjaichungwa@gmail.com>
2018-04-03 21:10:52 +0300

    Refactor :: move FloorOperator to core.floor module

---
+++ core/floor/floor_ops.py
@@ -0,0 +1,31 @@
+import bpy
+
+class FloorOperator(bpy.types.Operator):
+    """ Creates floors from active floorplan object """
+    bl_idname = "cynthia.add_floors"
+    bl_label = "Add Floors"
+    bl_options = {'REGISTER'}
+
+    @classmethod
+    def poll(cls, context):
+        return context.object is not None
+
+    def execute(self, context):
+        # Build geometry
+        floor = Floor()
+        floor.build(context)
+
+        # Add property list item
+        # -- prop.id is optional, only useful for collectiontypes
+        obj         = context.object
+        prop        = obj.property_list.add()
+        prop.id     = len(obj.property_list)
+        prop.type   = "FLOOR"
+        prop.name   = "Floor Property"
+
+        obj.property_index = len(obj.property_list)-1
+
+        # Add flag to be used in update
+        obj['has_floor'] = True
+
+        return {'FINISHED'}
