c5d3b69711d77c526d1bd364d27dae7a91bfa1e4
ranjian0 <karanjaichungwa@gmail.com>
2018-04-03 21:20:25 +0300

    Refactor :: move Floor to core.floor module

--- __init__.py
+++ __init__.py
@@ -718,33 +718,6 @@ class RoofProperty(bpy.types.PropertyGroup):
 #
 # =======================================================
 
-class FloorplanOperator(bpy.types.Operator):
-    """ Create a floorplan object """
-    bl_idname = "cynthia.add_floorplan"
-    bl_label = "Create Floorplan"
-    bl_options = {'REGISTER'}
-
-    def execute(self, context):
-        # Build the geometry
-        fp = Floorplan()
-        fp.build()
-
-        # Add property list item
-        # -- prop.id is optional, only useful for collectiontypes
-        obj         = context.object
-        prop        = obj.property_list.add()
-        prop.id     = len(obj.property_list)
-        prop.type   = "FLOORPLAN"
-        prop.name   = "Floorplan Property"
-
-        obj.property_index = len(obj.property_list)-1
-
-        # Add flag to be used in update
-        obj['has_floorplan'] = True
-
-        return {'FINISHED'}
-
-
 class WindowOperator(Template_Modal_OP):
     """ Creates windows on selected mesh faces """
     bl_idname = "cynthia.add_window"
