15ee9dde48395765c44a606633a92ea7ebfb97b5
ranjian0 <karanjaichungwa@gmail.com>
2018-04-08 23:33:51 +0300

    rename SplitProperty to SizeOffset

--- core/generic.py
+++ core/generic.py
@@ -2,36 +2,32 @@ import bpy
 from bpy.props import *
 
 
-class SplitProperty(bpy.types.PropertyGroup):
+class SizeOffsetProperty(bpy.types.PropertyGroup):
     """ Convinience PropertyGroup used for reqular Quad Inset
 
     TODO: rename this, Split implies dividing in half
     TODO: rename properties to size and position
     TODO: Clamp amount factor to between 0.0 and 1.0. (0.0 - 3.0 is confusing)
     """
-    amount  = FloatVectorProperty(
-        name="Split Amount", description="How much to split geometry", min=.01, max=2.99,
-        subtype='XYZ', size=2, default=(2.0, 2.7))
+    size  = FloatVectorProperty(
+        name="Size", min=.01, max=1.0, subtype='XYZ', size=2, default=(0.5, 0.7),
+        description="Size of geometry")
 
     off     = FloatVectorProperty(
-        name="Split Offset", description="How much to offset geometry", min=-1000.0, max=1000.0,
-        subtype='TRANSLATION', size=3, default=(0.0, 0.0, 0.0))
+        name="Offset", min=-1000.0, max=1000.0, subtype='TRANSLATION', size=3, default=(0.0, 0.0, 0.0),
+        description="How much to offset geometry")
 
     collapsed = BoolProperty()
 
     def draw(self, context, layout, parent):
         box = layout.box()
-        if parent.has_split:
-            row = box.row(align=True)
-            row.prop(parent, 'has_split', toggle=True)
-            row.prop(self, 'collapsed', text="",
-                icon='INLINK' if not self.collapsed else 'LINK')
+        row = box.row(align=True)
+        row.prop(self, 'collapsed', text="",
+            icon='INLINK' if not self.collapsed else 'LINK')
 
-            if not self.collapsed:
-                col = box.column(align=True)
-                col.prop(self, 'amount', slider=True)
+        if not self.collapsed:
+            col = box.column(align=True)
+            col.prop(self, 'size', slider=True)
 
-                col = box.column(align=True)
-                col.prop(self, 'off')
-        else:
-            box.prop(parent, 'has_split', toggle=True)
+            col = box.column(align=True)
+            col.prop(self, 'off')
