64e12ea9516d4ca2315f7ccae17451330d0d14d5
ranjian0 <karanjaichungwa@gmail.com>
2018-04-08 18:16:19 +0300

    Refactor:: remove object active_op register

--- core/register.py
+++ core/register.py
@@ -25,11 +25,7 @@ def register_core():
     for cls in classes:
         bpy.utils.register_class(cls)
 
-    bpy.types.Object.active_op = None
-
 
 def unregister_core():
     for cls in classes:
         bpy.utils.unregister_class(cls)
-
-    del bpy.types.Object.active_op
