1cc0784cbb595d08324f73c7bf63625de420fbbb
ranjian0 <karanjaichungwa@gmail.com>
2018-04-08 17:53:00 +0300

    Refactor:: remove floor materials

--- core/floor/floor_types.py
+++ core/floor/floor_types.py
@@ -7,9 +7,6 @@ from bmesh.types import (
 from ...utils import (
     get_edit_mesh
     filter_geom,
-    floor_mat_slab,
-    floor_mat_wall,
-    material_set_faces,
     )
 
 
@@ -24,26 +21,11 @@ def flr_multistorey(floor_count, floor_height, slab_thickness, slab_outset, **kw
         slab_outset (float): How much the slab extends outwards
     """
 
-    # -- make/get materials
-    slab_mat = kwargs.get("mat_slab")
-    wall_mat = kwargs.get("mat_wall")
-    if not slab_mat:
-        slab_mat = floor_mat_slab(obj)
-        obj.building.floors.mat_slab = slab_mat
-    if not wall_mat:
-        wall_mat = floor_mat_wall(obj)
-        obj.building.floors.mat_wall = wall_mat
-
-    mslab_faces, mfloor_faces = [], []
-
     # -- get active object bmesh
     bm = bmesh.from_edit_mesh(get_edit_mesh())
 
     # -- find boundary edges
     edges = [e for e in bm.edges if e.is_boundary]
-
-    # -- floorplan faces are slab material
-    mslab_faces.extend([f for f in bm.faces])
 
     # -- extrude floor and slabs
     for i in range(floor_count):
@@ -64,18 +46,9 @@ def flr_multistorey(floor_count, floor_height, slab_thickness, slab_outset, **kw
         # Offset Slab
         ret = bmesh.ops.inset_region(bm, faces=slab_faces, depth=-slab_outset)
 
-        # Material faces
-        mslab_faces.extend(slab_faces+ret['faces'])
-        mfloor_faces.extend(floor_faces)
 
     # -- fill top face
     ret = bmesh.ops.contextual_create(bm, geom=edges)
-    mslab_faces.extend(ret['faces'])
-
-    # Set materials
-    unique = lambda lst : list(set(lst))
-    material_set_faces(obj, slab_mat, unique(mslab_faces))
-    material_set_faces(obj, wall_mat, unique(mfloor_faces))
 
     # -- update normals and mesh
     bmesh.ops.recalc_face_normals(bm, faces=bm.faces)
