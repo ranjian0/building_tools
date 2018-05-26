b0c4b272d94f18178c7b9b904e8ed3becfbfabf2
ranjian0 <karanjaichungwa@gmail.com>
2018-05-26 20:59:54 +0300

    implement balcony open railings

--- core/balcony/balcony_props.py
+++ core/balcony/balcony_props.py
@@ -13,6 +13,17 @@ class BalconyProperty(bpy.types.PropertyGroup):
         name="Add Railing", default=True,
         description="Whether the balcony has railing")
 
+    open_items = [
+        ("NONE", "None", "", 0),
+        ("FRONT", "Front", "", 1),
+        ("LEFT", "Left", "", 2),
+        ("RIGHT", "Right", "", 3)
+    ]
+
+    open_side = EnumProperty(
+        name="Open Side", items=open_items, default='NONE',
+        description="Sides of the balcony with no railing")
+
     rail = PointerProperty(type=RailProperty)
     soff = PointerProperty(type=SizeOffsetProperty)
 
@@ -22,7 +33,8 @@ class BalconyProperty(bpy.types.PropertyGroup):
         row = layout.row()
         row.prop(self, 'width')
 
-        box = layout.box()
-        box.prop(self, 'railing', toggle=True)
+        layout.prop(self, 'railing', toggle=True)
         if self.railing:
+            box = layout.box()
+            box.prop(self, "open_side", text="Open")
             self.rail.draw(context, box)
--- core/balcony/balcony_types.py
+++ core/balcony/balcony_types.py
@@ -6,10 +6,11 @@ from ...utils import (
     split,
     filter_geom,
     get_edit_mesh,
+    calc_edge_median,
     )
 
 
-def make_balcony(width, railing, size, off, **kwargs):
+def make_balcony(width, railing, size, off, open_side, **kwargs):
     """Generate balcony geometry
 
     Args:
@@ -32,9 +33,26 @@ def make_balcony(width, railing, size, off, **kwargs):
             top_verts = sorted(list(face.verts), key=lambda v:v.co.z)[2:]
             edges     = list({e for v in top_verts for e in v.link_edges
                             if e not in list(face.edges)})
-            edges.append(bm.edges.get(top_verts))
 
-            rails.make_railing(bm, edges, **kwargs)
+            if f.normal.y:
+                edges.sort(key=lambda e:calc_edge_median(e).x, reverse=f.normal.y < 0)
+            elif f.normal.x:
+                edges.sort(key=lambda e:calc_edge_median(e).y, reverse=f.normal.x > 0)
+            left, right = edges
+
+            front = bm.edges.get(top_verts)
+
+            r_edges = []
+            if open_side == 'NONE':
+                r_edges = [left, right, front]
+            elif open_side == 'FRONT':
+                r_edges = [left, right]
+            elif open_side == 'LEFT':
+                r_edges = [front, right]
+            elif open_side == 'RIGHT':
+                r_edges = [front, left]
+
+            rails.make_railing(bm, r_edges, **kwargs)
 
         bmesh.ops.delete(bm, geom=[f], context=3)
     bmesh.update_edit_mesh(me, True)
