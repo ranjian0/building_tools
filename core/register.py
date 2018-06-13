390f9b8569e8482d415f90e9b528c2742e4dda4f
ranjian0 <karanjaichungwa@gmail.com>
2018-06-13 13:39:52 +0300

    reimplement rail, balcony and stair registration

--- core/register.py
+++ core/register.py
@@ -1,11 +1,14 @@
 import bpy
 
 from .util_fill import register_fill, unregister_fill
+from .util_rail import register_rail, unregister_rail
 
 from .door      import register_door, unregister_door
 from .floor     import register_floor, unregister_floor
 from .window    import register_window, unregister_window
 from .floorplan import register_floorplan, unregister_floorplan
+from .balcony   import register_balcony, unregister_balcony
+from .stairs    import register_stairs, unregister_stairs
 from .generic   import register_generic, unregister_generic
 
 # -- take care here --
@@ -14,21 +17,27 @@ from .generic   import register_generic, unregister_generic
 register_funcs = [
     register_generic,
     register_fill,
+    register_rail,
 
     register_door,
     register_floor,
     register_window,
     register_floorplan,
+    register_balcony,
+    register_stairs
 ]
 
 unregister_funcs = [
     unregister_generic,
     unregister_fill,
+    unregister_rail,
 
     unregister_door,
     unregister_floor,
     unregister_window,
     unregister_floorplan,
+    unregister_balcony,
+    unregister_stairs
 ]
 
 
