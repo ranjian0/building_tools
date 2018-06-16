aec674b377b661b7a74422a1a9a6cfa53bfc697d
ranjian0 <karanjaichungwa@gmail.com>
2018-06-16 15:01:45 +0300

    rails - remove deprecate invalid selection condition

--- core/util_rail/rails.py
+++ core/util_rail/rails.py
@@ -15,10 +15,11 @@ class Rails:
         bm = bmesh.from_edit_mesh(me)
 
         # if a face is selected that is not upward facing, return
-        faces = [f for f in bm.faces if f.select]
-        if faces:
-            if not all([f.normal.z == 1 for f in faces]):
-                return {'CANCELLED'}
+        if False
+            faces = [f for f in bm.faces if f.select]
+            if faces:
+                if not all([f.normal.z == 1 for f in faces]):
+                    return {'CANCELLED'}
 
         edges = [e for e in bm.edges if e.select]
         make_railing(bm, edges, **kwargs_from_props(props))
