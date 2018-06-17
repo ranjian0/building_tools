c7f10a2931502504ae0e0115c4b80196e2dccd2a
ranjian0 <karanjaichungwa@gmail.com>
2018-06-17 13:21:39 +0300

    remove unused arg

--- core/balcony/balcony.py
+++ core/balcony/balcony.py
@@ -22,14 +22,14 @@ class Balcony:
         bm = bmesh.from_edit_mesh(me)
         faces = [face for face in bm.faces if face.select]
 
-        if cls.validate(bm, faces):
+        if cls.validate(faces):
             make_balcony(bm, faces, **kwargs_from_props(props))
             bmesh.update_edit_mesh(me, True)
             return {'FINISHED'}
         return {'CANCELLED'}
 
     @classmethod
-    def validate(cls, bm, faces):
+    def validate(cls, faces):
         """ Ensure user has appropriate selection if any """
         if faces:
             # -- ensure all are upward facing
