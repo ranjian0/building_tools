97dd38332c0bc0dd6c2ce6539a377b7b49834369
ranjian0 <karanjaichungwa@gmail.com>
2018-06-17 13:06:54 +0300

    add validation to Rails.build

--- core/util_rail/rails.py
+++ core/util_rail/rails.py
@@ -11,18 +11,17 @@ class Rails:
 
     @classmethod
     def build(cls, context, props):
-        me = get_edit_mesh()
-        bm = bmesh.from_edit_mesh(me)
-
-        # if a face is selected that is not upward facing, return
-        if False:
-            faces = [f for f in bm.faces if f.select]
-            if faces:
-                if not all([f.normal.z == 1 for f in faces]):
-                    return {'CANCELLED'}
+        faces = [f for f in bm.faces if f.select]
+        if faces:
+            if not all([f.normal.z == 1.0 for f in faces]):
+                return {'CANCELLED'}
 
         edges = [e for e in bm.edges if e.select]
-        make_railing(bm, edges, **kwargs_from_props(props))
-        bmesh.update_edit_mesh(me, True)
+        if edges:
+            me = get_edit_mesh()
+            bm = bmesh.from_edit_mesh(me)
 
-        return {'FINISHED'}
+            make_railing(bm, edges, **kwargs_from_props(props))
+            bmesh.update_edit_mesh(me, True)
+            return {'FINISHED'}
+        return {'CANCELLED'}
