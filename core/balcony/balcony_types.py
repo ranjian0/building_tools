315e4fefb409d44ff742811442cfb3797c0ce880
ranjian0 <karanjaichungwa@gmail.com>
2018-06-18 10:21:20 +0300

    balcony: mute railing

--- core/balcony/balcony_types.py
+++ core/balcony/balcony_types.py
@@ -49,6 +49,6 @@ def make_balcony(bm, faces, width, railing, size, off, open_side, **kwargs):
             elif open_side == 'RIGHT':
                 r_edges = [front, left]
 
-            rails.make_railing(bm, r_edges, **kwargs)
+            # rails.make_railing(bm, r_edges, **kwargs)
 
         bmesh.ops.delete(bm, geom=[f], context=3)
