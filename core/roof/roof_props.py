8e3eab4751033f50b867cf87995a978cb0d32354
ranjian0 <karanjaichungwa@gmail.com>
2018-07-03 10:19:33 +0300

    add initial roof properties

--- core/roof/roof_props.py
+++ core/roof/roof_props.py
@@ -2,6 +2,52 @@ import bpy
 from bpy.props import *
 
 class RoofProperty(bpy.types.PropertyGroup):
+    roof_items = [
+        ("FLAT", "Flat", "", 0),
+        ("GABLE", "Gable", "", 1),
+        ("HIP", "Hip", "", 2),
+    ]
+    type = EnumProperty(
+        name="Roof Type", items=roof_items, default='FLAT'
+        description="Type of roof to create")
+
+    thick = FloatProperty(
+        name="Thickness",  min=0.01, max=1000.0, default=.1,
+        description="Thickness of roof hangs")
+
+    outset = FloatProperty(
+        name="Outset",  min=0.01, max=1000.0, default=.1,
+        description="Outset of roof hangs")
+
+    height = FloatProperty(
+        name="Height",  min=0.01, max=1000.0, default=1,
+        description="Height of entire roof")
+
+    # o_types = [("LEFT", "Left", "", 0), ("RIGHT", "Right", "", 1), ]
+    # orient = EnumProperty(description="Orientation of gable", items=o_types, default='LEFT')
+
 
     def draw(self, context, layout):
-        pass
+        layout.prop(self, 'type', text="")
+
+        box = layout.box()
+        if self.type == 'FLAT':
+            col = box.column()
+            col.prop(self, 'thick')
+            col.prop(self, 'outset')
+
+        elif self.type == 'GABLE':
+            col = box.column()
+            col.prop(self, 'thick')
+            col.prop(self, 'outset')
+            col.prop(self, 'height')
+
+            # row = box.row(align=True)
+            # row.prop(self, 'orient', expand=True)
+
+        else:
+            col = box.column()
+            col.prop(self, 'thick')
+            col.prop(self, 'outset')
+
+            col.prop(self, 'height')
