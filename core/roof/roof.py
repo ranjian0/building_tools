cddb61d194d333926d4edb60ff8f91ce6a6da0f3
ranjian0 <karanjaichungwa@gmail.com>
2018-07-04 10:28:26 +0300

    roof add validation

--- core/roof/roof.py
+++ core/roof/roof.py
@@ -1,8 +1,29 @@
 import bpy
 import bmesh
 
+from ...utils import (
+    get_edit_mesh,
+    kwargs_from_props
+    )
+
 class Roof:
 
     @classmethod
     def build(cls, context, props):
-        return {'FINISHED'}
+        me = get_edit_mesh()
+        bm = bmesh.from_edit_mesh(me)
+
+        if cls.validate(bm):
+
+            bmesh.update_edit_mesh(me, True)
+            return {'FINISHED'}
+        return {'CANCELLED'}
+
+    @classmethod
+    def validate(cls, bm):
+        """ Ensure valid user selection if any """
+        faces = [f for f in bm.faces if f.select]
+        if faces:
+            if all([f.normal.z for f in faces]):
+                return True
+        return False
