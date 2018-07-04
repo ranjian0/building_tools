4817c0dafbba23016d0da50291f9a5be93da95b6
ranjian0 <karanjaichungwa@gmail.com>
2018-07-04 10:33:29 +0300

    add make_roof

--- core/roof/roof.py
+++ core/roof/roof.py
@@ -1,6 +1,7 @@
 import bpy
 import bmesh
 
+from .roof_types import make_roof
 from ...utils import (
     get_edit_mesh,
     kwargs_from_props
@@ -12,9 +13,10 @@ class Roof:
     def build(cls, context, props):
         me = get_edit_mesh()
         bm = bmesh.from_edit_mesh(me)
+        faces = [f for f in bm.faces if f.select]
 
         if cls.validate(bm):
-
+            make_roof(bm, faces, **kwargs_from_props(props))
             bmesh.update_edit_mesh(me, True)
             return {'FINISHED'}
         return {'CANCELLED'}
---
+++ core/roof/roof_types.py
@@ -0,0 +1,11 @@
+import bpy
+import bmesh
+
+def make_roof(bm, faces, type, **kwargs):
+
+    if type == 'FLAT':
+        pass
+    elif type == 'GABLE':
+        pass
+    elif type == 'HIP':
+        pass
