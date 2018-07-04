d06d8daa702a701658e915a5a1f3b511acd084b5
ranjian0 <karanjaichungwa@gmail.com>
2018-07-04 10:52:59 +0300

    implement flat roof

--- core/roof/roof_types.py
+++ core/roof/roof_types.py
@@ -1,8 +1,14 @@
 import bpy
 import bmesh
 
-def make_roof(bm, faces, type, **kwargs):
+from bmesh.types import BMVert, BMEdge, BMFace
+from ...utils import (
+    select,
+    filter_geom,
+    )
 
+def make_roof(bm, faces, type, **kwargs):
+    select(faces, False)
     if type == 'FLAT':
         make_flat_roof(bm, faces, **kwargs)
     elif type == 'GABLE':
@@ -10,8 +16,24 @@ def make_roof(bm, faces, type, **kwargs):
     elif type == 'HIP':
         make_hip_roof(bm, faces, **kwargs)
 
-def make_flat_roof(bm, faces, **kwargs):
-    pass
+def make_flat_roof(bm, faces, thick, outset, **kwargs):
+
+    ret = bmesh.ops.extrude_face_region(bm, geom=faces)
+    bmesh.ops.translate(bm,
+        vec=(0, 0, thick),
+        verts=filter_geom(ret['geom'], BMVert))
+
+    top_face = filter_geom(ret['geom'], BMFace)[-1]
+    link_faces = [f for e in top_face.edges for f in e.link_faces
+                    if f is not top_face]
+
+    bmesh.ops.inset_region(bm, faces=link_faces, depth=outset)
+    bmesh.ops.recalc_face_normals(bm, faces=bm.faces)
+
+    bmesh.ops.delete(bm,
+        geom=faces,
+        context=5)
+
 
 def make_gable_roof(bm, faces, **kwargs):
     pass
