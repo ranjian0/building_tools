56bc710755ecc34e50bf1b09a4148d7b485207de
ranjian0 <karanjaichungwa@gmail.com>
2018-09-29 15:30:11 +0300

    finish gable roof type

--- core/roof/roof_types.py
+++ core/roof/roof_types.py
@@ -66,6 +66,42 @@ def make_gable_roof(bm, faces, thick, outset, height, orient, **kwargs):
         setattr(v.co, axis, mid)
     bmesh.ops.remove_doubles(bm, verts=bm.verts)
 
+    # -- offset roof boundary
+    maxz = max([v.co.z for v in bm.verts])
+    top_verts = [v for v in bm.verts if v.co.z == maxz]
+    faces = list(set([f for v in top_verts for f in v.link_faces if f.normal.z]))
+    boundary_edges = [e for f in faces for e in f.edges
+        if len(set(e.link_faces) - set(faces)) == 1]
+    bmesh.ops.delete(bm, geom=faces, context=5)
+
+    ret = bmesh.ops.extrude_edge_only(bm, edges=boundary_edges)
+    verts = filter_geom(ret['geom'], BMVert)
+    bmesh.ops.scale(bm, verts=verts, vec=(1 + outset, 1 + outset, 1))
+    nedges = list(set([e for v in verts for e in v.link_edges if e.is_boundary]))
+
+    # -- fix roof slope at bottom edges
+    min_edges = [e for e in nedges
+        if calc_edge_median(e).z == min([v.co.z for e in nedges for v in e.verts])]
+    bmesh.ops.translate(bm,
+        verts=list(set([v for e in min_edges for v in e.verts])),
+        vec=(0, 0, -outset))
+
+    # -- extrude edges upwards and fill face
+    ret = bmesh.ops.extrude_edge_only(bm, edges=nedges)
+    verts = filter_geom(ret['geom'], BMVert)
+    edges = filter_geom(ret['geom'], BMEdge)
+    bmesh.ops.translate(bm, verts=verts, vec=(0, 0, thick))
+
+    valid_edges = [e for e in edges
+        if calc_edge_median(e).z != min([v.co.z for e in edges for v in e.verts])]
+    edge_loc = set([getattr(calc_edge_median(e), axis) for e in valid_edges])
+
+    # -- fill faces
+    for loc in edge_loc:
+        edges = [e for e in valid_edges if getattr(calc_edge_median(e), axis) == loc]
+        bmesh.ops.contextual_create(bm, geom=edges)
+    bmesh.ops.recalc_face_normals(bm, faces=bm.faces)
+
 
 def make_hip_roof(bm, faces, **kwargs):
     pass
