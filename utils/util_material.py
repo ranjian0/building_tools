de9fe18d7d560f3de381f3572b940060e1788f7b
ranjian0 <karanjaichungwa@gmail.com>
2019-04-27 20:23:08 +0300

    new simple material API

--- utils/util_material.py
+++ utils/util_material.py
@@ -13,16 +13,12 @@ def create_mat(name="Default Material"):
 		MAT_WRAPPER_CACHE[mat] = mat_wrap
 	return mat
 
-def link_mat(obj, mat):
-	if not has_material(obj, mat.name):
-		obj.data.materials.append(mat)
-
 def create_material_group(obj, group_name):
 	# create material
 	mat = create_mat(group_name+"_mat")
 	link_mat(obj, mat)
 
-	# Create group in none exists
+	# Create group if none exists
 	if group_name not in [g.name for g in obj.mat_groups]:
 		group = obj.mat_groups.add()
 		group.name = group_name
@@ -41,13 +37,10 @@ def material_set_faces(obj, mat, faces):
 	for face in faces:
 		face.material_index = mat_index
 
-def has_material(obj, name):
-	return name in obj.data.materials.keys()
 
-
-DEFAULT_BASE_COLORS = {
-	"mat_slab" : (.4, .35, .3),
-	"mat_wall" : (.3, .25, .13),
+DEFAULT_MATERIALS = {
+	"mat_slab" : (.208, .183, .157),
+	"mat_wall" : (.190, .117, .04),
 
 	"mat_window_frame" : (.8, .8, .8),
 	"mat_window_pane"  : (0, .6, 0),
@@ -59,3 +52,33 @@ DEFAULT_BASE_COLORS = {
 	"mat_door_groove" : (.13, .05, 0),
 	"mat_door_glass"  : (0, .1, .6)
 }
+
+def create_default_materials(obj):
+	for name, color in DEFAULT_MATERIALS.items():
+		mat = bpy.data.materials.new(obj.name + '_' + name)
+		mat.diffuse_color = color + (1,)
+		mat.use_nodes = True
+		link_mat(obj, mat)
+
+def link_mat(obj, mat):
+	if not has_material(obj, mat.name):
+		obj.data.materials.append(mat)
+
+def has_material(obj, name):
+	return name in obj.data.materials.keys()
+
+def set_material(faces, name_or_index):
+	obj = bpy.context.object
+	if not obj: return
+
+	mat_idx = 0
+	if isinstance(name_or_index, str):
+		name = name_or_index
+		for i, mat in enumerate(obj.data.materials):
+			if name in mat.name:
+				mat_idx = i
+	elif isinstance(name_or_index, int):
+		mat_idx = name_or_index
+
+	for f in faces:
+		f.material_index = mat_idx
