8d780f3f83e2682426e21ab70428e8b62345fb9d
ranjian0 <karanjaichungwa@gmail.com>
2019-04-27 20:37:02 +0300

    remove old material API

--- utils/util_material.py
+++ utils/util_material.py
@@ -1,42 +1,4 @@
 import bpy
-from bpy_extras.node_shader_utils import PrincipledBSDFWrapper
-
-# XXX Cache for holding PrincipledBSDFWrappers for all materials
-MAT_WRAPPER_CACHE = {}
-
-def create_mat(name="Default Material"):
-	mat = bpy.data.materials.get(name, bpy.data.materials.new(name))
-	mat_wrap = MAT_WRAPPER_CACHE.get(mat, None)
-	if not mat_wrap:
-		mat_wrap = PrincipledBSDFWrapper(mat, is_readonly=False)
-		mat_wrap.use_nodes = True
-		MAT_WRAPPER_CACHE[mat] = mat_wrap
-	return mat
-
-def create_material_group(obj, group_name):
-	# create material
-	mat = create_mat(group_name+"_mat")
-	link_mat(obj, mat)
-
-	# Create group if none exists
-	if group_name not in [g.name for g in obj.mat_groups]:
-		group = obj.mat_groups.add()
-		group.name = group_name
-		group.material = mat
-		obj.mat_group_index = len(obj.mat_groups)-1
-
-def get_material_wrapper(mat):
-	return MAT_WRAPPER_CACHE.get(mat, PrincipledBSDFWrapper(mat, is_readonly=False))
-
-def material_set_faces(obj, mat, faces):
-	# if the material is not in obj.materials, append it
-	if not mat: return
-	if mat.name not in obj.data.materials:
-		link_mat(obj, mat)
-	mat_index = list(obj.data.materials).index(mat)
-	for face in faces:
-		face.material_index = mat_index
-
 
 DEFAULT_MATERIALS = {
 	"mat_slab" : (.208, .183, .157),
