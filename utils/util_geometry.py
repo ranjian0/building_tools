334a45ac1bf772f0ef002d4f7d9b601542689a19
ranjian0 <karanjaichungwa@gmail.com>
2019-05-15 22:00:41 +0300

    refactor util_geometry

--- utils/util_geometry.py
+++ utils/util_geometry.py
@@ -9,8 +9,7 @@ def cube(bm, width=2, length=2, height=2):
     sc_y = Matrix.Scale(length, 4, (0, 1, 0))
     sc_z = Matrix.Scale(height, 4, (0, 0, 1))
     mat = sc_x @ sc_y @ sc_z
-    ret = bmesh.ops.create_cube(bm, size=1, matrix=mat)
-    return ret
+    return bmesh.ops.create_cube(bm, size=1, matrix=mat)
 
 
 def plane(bm, width=2, length=2):
@@ -19,21 +18,19 @@ def plane(bm, width=2, length=2):
     sc_x = Matrix.Scale(width, 4, (1, 0, 0))
     sc_y = Matrix.Scale(length, 4, (0, 1, 0))
     mat = sc_x @ sc_y
-    ret = bmesh.ops.create_grid(bm, x_segments=1, y_segments=1, size=1, matrix=mat)
-    return ret
+    return bmesh.ops.create_grid(bm, x_segments=1, y_segments=1, size=1, matrix=mat)
 
 
 def circle(bm, radius=1, segs=10, cap_tris=False):
     """ Create circle in the bmesh """
-    ret = bmesh.ops.create_circle(
+    return bmesh.ops.create_circle(
         bm, cap_ends=True, cap_tris=cap_tris, segments=segs, radius=radius
     )
-    return ret
 
 
 def cone(bm, r1=0.5, r2=0.01, height=2, segs=32):
     """ Create a cone in the bmesh """
-    ret = bmesh.ops.create_cone(
+    return bmesh.ops.create_cone(
         bm,
         diameter1=r1 * 2,
         diameter2=r2 * 2,
@@ -42,23 +39,22 @@ def cone(bm, r1=0.5, r2=0.01, height=2, segs=32):
         cap_tris=True,
         segments=segs,
     )
-    return ret
 
 
 def cylinder(bm, radius=1, height=2, segs=10):
     """ Create cylinder in bmesh """
 
     # -- circle
-    ret = bmesh.ops.create_circle(
+    circle = bmesh.ops.create_circle(
         bm, cap_ends=True, cap_tris=False, segments=segs, diameter=radius * 2
     )
 
-    verts = ret["verts"]
+    verts = circle["verts"]
     face = list(verts[0].link_faces)
 
-    res = bmesh.ops.extrude_discrete_faces(bm, faces=face)
-    bmesh.ops.translate(bm, verts=res["faces"][-1].verts, vec=(0, 0, height))
+    cylinder = bmesh.ops.extrude_discrete_faces(bm, faces=face)
+    bmesh.ops.translate(bm, verts=cylinder["faces"][-1].verts, vec=(0, 0, height))
 
-    result = {"verts": verts + list(res["faces"][-1].verts)}
+    result = {"verts": verts + list(cylinder["faces"][-1].verts)}
     bmesh.ops.translate(bm, verts=result["verts"], vec=(0, 0, -height / 2))
     return result
