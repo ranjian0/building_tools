b802902ac0d14b963e9ce2ffd764bccb31fc3842
ranjian0 <karanjaichungwa@gmail.com>
2019-05-15 22:06:10 +0300

    refactor util_common

--- utils/util_common.py
+++ utils/util_common.py
@@ -7,19 +7,19 @@ def equal(a, b, eps=0.001):
     return a == b or (abs(a - b) <= max(abs(a), abs(b)) * eps)
 
 
-def clamp(val, _min, _max):
-    """ Reset val between _min and __max """
-    return max(min(val, _max), _min)
+def clamp(value, minimum, maximum):
+    """ Reset value between minimum and maximum """
+    return max(min(value, maximum), minimum)
 
 
-def condition(con, val1, val2):
-    """ Return val1 if con is true else val2 """
-    return val1 if con else val2
+def condition(condition, value_true, value_false):
+    """ Return value_true if condition is True else value_false """
+    return value_true if condition else value_false
 
 
-def ifeven(num, val1, val2):
-    """ Return val1 if num is an even number else val2 """
-    return condition(num % 2 == 0, val1, val2)
+def ifeven(number, value_even, value_odd):
+    """ Return value_even if number is an even number else value_odd """
+    return condition(number % 2 == 0, value_even, value_odd)
 
 
 def args_from_props(props, names):
@@ -31,10 +31,10 @@ def kwargs_from_props(props):
     """ Converts all properties in a props{bpy.types.PropertyGroup} into dict """
     valid_types = (
         int,
-        float,
         str,
-        tuple,
         bool,
+        float,
+        tuple,
         Vector,
         bpy.types.Material,
         bpy.types.Object,
@@ -44,8 +44,8 @@ def kwargs_from_props(props):
     for p in dir(props):
         if p.startswith("__") or p in ["rna_type", "bl_rna"]:
             continue
-        prop = getattr(props, p)
 
+        prop = getattr(props, p)
         if isinstance(prop, valid_types):
             result[p] = prop
         elif isinstance(prop, bpy.types.PropertyGroup) and not isinstance(
@@ -54,30 +54,3 @@ def kwargs_from_props(props):
             # property group within this property
             result.update(kwargs_from_props(prop))
     return result
-
-
-def assert_test(func):
-    """ Catch any Exceptions that may occur in test func """
-
-    def wrapper():
-        try:
-            func()
-            print(func.__name__.upper() + " PASSED ..")
-        except Exception as e:
-            print(func.__name__.upper() + " FAILED .. :", e)
-
-    return wrapper
-
-
-def clean_scene():
-    """ Delete all objects in the scene if any """
-    scene = bpy.context.scene
-
-    if scene.collection.objects:
-        active = scene.collection.objects.active
-        if active and active.mode == "EDIT":
-            bpy.ops.object.mode_set(mode="OBJECT")
-
-        bpy.ops.object.select_all(action="SELECT")
-        bpy.ops.object.delete(use_global=False)
-    return scene
