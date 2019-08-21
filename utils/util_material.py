bf214c54caadc5d6609814670985f8b8b0d220bf
ranjian0 <karanjaichungwa@gmail.com>
2019-08-21 20:12:48 +0300

    Material System: add facegroups Enum and functions

--- utils/util_material.py
+++ utils/util_material.py
@@ -1,15 +1,51 @@
 import bpy
+import bmesh
 from enum import Enum
+from functools import wraps
+
+from .util_object import bm_from_obj, bm_to_obj
 
 
-class Material(Enum):
-    SLAB = "mat_slab"
-    WALL = "mat_wall"
+class FaceGroups(Enum):
+    SLAB = 0
+    WALL = 1
+
+
+def facegroup(group):
+    def outer(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            bm = [arg for arg in args if isinstance(arg, bmesh.types.BMesh)].pop()
+            faces = set(bm.faces)
+
+            func(*args, **kwargs)
+
+            new_faces = set(bm.faces) - faces
+            add_faces_to_group(bm, new_faces, group)
+        return wrapper
+    return outer
+
+
+def add_faces_to_group(bm, faces, group):
+    face_map = bm.faces.layers.face_map.active
+    for face in faces:
+        face[face_map] = group.value
+
+
+def create_face_maps_for_object(obj):
+    # -- verify face map
+    bm = bm_from_obj(obj)
+    bm.faces.layers.face_map.verify()
+    bm_to_obj(bm, obj)
+
+    # -- add face maps
+    for group in FaceGroups:
+        obj.face_maps.new(name=group.name.lower())
 
 
 DEFAULT_MATERIALS = {
-    Material.SLAB.value: (0.208, 0.183, 0.157),
-    Material.WALL.value: (0.190, 0.117, 0.04),
+    "mat_slab": (0.208, 0.183, 0.157),
+    "mat_wall": (0.190, 0.117, 0.04),
     "mat_window_frame": (0.8, 0.8, 0.8),
     "mat_window_pane": (0, 0.6, 0),
     "mat_window_bars": (0, 0.7, 0),
