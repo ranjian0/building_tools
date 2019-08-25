b94fefd4f0ec1eda7315aa7db8060b7692badcd0
ranjian0 <karanjaichungwa@gmail.com>
2019-08-25 21:14:05 +0300

    Stair fixes:
    refactor into multiple methods and change order of operations to subdivide first
    and then extrude
    
    also calculate proper offsets for steps

--- core/stairs/stairs_types.py
+++ core/stairs/stairs_types.py
@@ -24,43 +24,56 @@ def create_stairs(bm, faces, prop):
         f = create_stair_split(bm, f, prop.size_offset)
 
         add_faces_to_map(bm, [f], FaceMap.STAIRS)
-        get_z = operator.attrgetter("co.z")
-        fheight = max(f.verts, key=get_z).co.z - min(f.verts, key=get_z).co.z
 
         # -- options for railing
         top_faces = []
         init_normal = f.normal.copy()
 
-        ext_face = f
-        step_count = prop.step_count + (1 if prop.landing else 0)
-        for i in range(step_count):
-            ret_face = extrude_step(bm, i, ext_face, prop)
-
-            # -- keep reference to top faces for railing
-            faces = {f for e in ret_face.edges for f in e.link_faces if f.normal.z > 0}
-            top_faces.append(list(faces).pop())
-
-            if prop.landing and i == 0:
-                ret_face = get_stair_face_from_direction(bm, ret_face, prop)
-
-            if i < (step_count - 1) and not (prop.landing and i == 0):
-                ext_face = subdivide_next_step(bm, ret_face, fheight, step_count, i)
-                continue
-            ext_face = ret_face
+        f = create_landing(bm, f, top_faces, prop)
+        create_steps(bm, f, top_faces, prop)
 
     if prop.railing:
         create_stairs_railing(bm, init_normal, top_faces, prop)
 
 
-def extrude_step(bm, step_idx, face, prop):
+def create_landing(bm, f, top_faces, prop):
+    """ Create stair landing """
+    if prop.landing:
+        ret_face = extrude_step(bm, f, prop.landing_width)
+
+        # -- keep reference to top faces for railing
+        faces = {f for e in ret_face.edges for f in e.link_faces if f.normal.z > 0}
+        top_faces.append(list(faces).pop())
+
+        return get_stair_face_from_direction(bm, ret_face, prop)
+    return f
+
+
+def create_steps(bm, f, top_faces, prop):
+    """ Create stair steps """
+    ext_face = f
+    get_z = operator.attrgetter("co.z")
+    fheight = max(f.verts, key=get_z).co.z - min(f.verts, key=get_z).co.z
+
+    step_size = fheight / (prop.step_count + 1)
+    start_loc = max(f.verts, key=get_z).co.z
+    for i in range(prop.step_count):
+        offset = start_loc - (step_size * (i+1))
+        ret_face = subdivide_next_step(bm, ext_face, offset)
+
+        ext_face = extrude_step(bm, ret_face, prop.step_width)
+
+        # -- keep reference to top faces for railing
+        faces = {f for e in ext_face.edges for f in e.link_faces if f.normal.z > 0}
+        top_faces.append(list(faces).pop())
+
+
+def extrude_step(bm, face, step_width):
     """ Extrude a stair step
     """
     n = face.normal
-    ext_width = (
-        prop.landing_width if (prop.landing and step_idx == 0) else prop.step_width
-    )
     ret_face = bmesh.ops.extrude_discrete_faces(bm, faces=[face]).get("faces").pop()
-    bmesh.ops.translate(bm, vec=n * ext_width, verts=ret_face.verts)
+    bmesh.ops.translate(bm, vec=n * step_width, verts=ret_face.verts)
     return ret_face
 
 
@@ -82,14 +95,13 @@ def get_stair_face_from_direction(bm, ret_face, prop):
     return {"LEFT": left, "RIGHT": right}.get(prop.stair_direction, ret_face)
 
 
-def subdivide_next_step(bm, ret_face, fheight, step_count, step_idx):
+def subdivide_next_step(bm, ret_face, offset):
     """ cut the next face step height
     """
     res = subdivide_face_edges_horizontal(bm, ret_face, cuts=1)
     verts = filter_geom(res["geom_inner"], BMVert)
-    bmesh.ops.translate(
-        bm, verts=verts, vec=(0, 0, (fheight / 2) - (fheight / (step_count - step_idx)))
-    )
+    for v in verts:
+        v.co.z = offset
     return min(verts.pop().link_faces, key=lambda f: f.calc_center_median().z)
 
 
