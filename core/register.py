a8ce9315268e496209def7ba1e594ec66406a4d0
ranjian0 <karanjaichungwa@gmail.com>
2020-01-08 18:21:30 +0300

    REMOVE BALCONY AND RAILS REGISTRATION FUNCS

--- core/register.py
+++ core/register.py
@@ -1,11 +1,9 @@
 from .fill import register_fill, unregister_fill
 from .door import register_door, unregister_door
 from .roof import register_roof, unregister_roof
-from .rails import register_rail, unregister_rail
 from .floor import register_floor, unregister_floor
 from .stairs import register_stairs, unregister_stairs
 from .window import register_window, unregister_window
-from .balcony import register_balcony, unregister_balcony
 from .generic import register_generic, unregister_generic
 from .floorplan import register_floorplan, unregister_floorplan
 
@@ -14,12 +12,10 @@ from .floorplan import register_floorplan, unregister_floorplan
 register_funcs = (
     register_generic,
     register_fill,
-    register_rail,
     register_door,
     register_floor,
     register_window,
     register_floorplan,
-    register_balcony,
     register_stairs,
     register_roof,
 )
@@ -27,12 +23,10 @@ register_funcs = (
 unregister_funcs = (
     unregister_generic,
     unregister_fill,
-    unregister_rail,
     unregister_door,
     unregister_floor,
     unregister_window,
     unregister_floorplan,
-    unregister_balcony,
     unregister_stairs,
     unregister_roof,
 )
