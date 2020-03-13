96e47663b903cc03d5fa5a39a5c3a217b0bb5f8d
Lucky Kadam <luckykadam94@gmail.com>
2020-03-14 00:17:25 +0530

    support array on window and door

--- core/window/window_props.py
+++ core/window/window_props.py
@@ -63,7 +63,7 @@ class WindowProperty(bpy.types.PropertyGroup):
 
     def init(self, wall_dimensions):
         self.wall_dimensions = wall_dimensions
-        self.size_offset.parent_dimensions = self.wall_dimensions
+        self.size_offset.parent_dimensions = (self.wall_dimensions.x/self.array.count, self.wall_dimensions.y)
 
     def draw(self, context, layout):
         self.size_offset.draw(context, layout)
