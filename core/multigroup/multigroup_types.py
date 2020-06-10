61cb8c543ca94acef8d8635b5627bf055b3aee80
Marcus Elg <marcusaccounts@yahoo.se>
2020-06-10 10:50:20 +0200

    Prevent error with invalid chars in multigroup components

--- core/multigroup/multigroup_types.py
+++ core/multigroup/multigroup_types.py
@@ -1,4 +1,5 @@
 import bmesh
+import re
 from ..frame import add_frame_depth
 from ..window.window_types import fill_window_face
 
@@ -36,6 +37,11 @@ def create_multigroup(bm, faces, prop):
     # Prevent error when there are no components
     if len(str(prop.components)) == 0:
         popup_message("No components are chosen", "No Components Error")
+        return False
+
+    # Prevent error when there are invalid chars
+    if not re.match("^[dw]*$", str(prop.components)):
+        popup_message("Components cannot contain chars other than d and w", "Invalid Characters Error")
         return False
 
     for face in faces:
