f69f442c4269104ddf22dd4d69ea1d847d02741c
ranjian0 <karanjaichungwa@gmail.com>
2021-02-06 10:57:28 +0300

    unfreeze add custom obj

--- btools/__init__.py
+++ btools/__init__.py
@@ -66,9 +66,9 @@ class BTOOLS_PT_building_tools(bpy.types.Panel):
         col.operator("btools.add_multigroup")
         col.operator("btools.add_fill")
 
-        # col = layout.column(align=True)
-        # col.operator("btools.add_custom")
-        # col.prop(context.scene, "btools_custom_object", text="")
+        col = layout.column(align=True)
+        col.operator("btools.add_custom")
+        col.prop(context.scene, "btools_custom_object", text="")
 
         layout.separator()
         layout.operator("btools.remove_geom")
