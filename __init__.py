41410463844620abd169eaa97e24c30d90a2bab4
ranjian0 <karanjaichungwa@gmail.com>
2021-05-22 17:42:31 +0300

    implement addon updater. Closes #186

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import bpy
 
+from . import addon_updater_ops
 from .btools.building.register import register_building, unregister_building
 
 bl_info = {
@@ -141,18 +142,64 @@ class BTOOLS_PT_material_tools(bpy.types.Panel):
             layout.template_ID_preview(face_map_material, "material", hide_buttons=True)
 
 
-classes = (BTOOLS_PT_building_tools, BTOOLS_PT_material_tools)
+class BTOOLS_PT_PrefPanel(bpy.types.AddonPreferences):
+    bl_idname = __name__
+
+    # addon updater preferences
+    auto_check_update : bpy.props.BoolProperty(
+        name="Auto-check for Update",
+        description="If enabled, auto-check for updates using an interval",
+        default=False,
+    )
+
+    updater_intrval_months : bpy.props.IntProperty(
+        name='Months',
+        description="Number of months between checking for updates",
+        default=0,
+        min=0
+    )
+    updater_intrval_days : bpy.props.IntProperty(
+        name='Days',
+        description="Number of days between checking for updates",
+        default=7,
+        min=0,
+    )
+    updater_intrval_hours : bpy.props.IntProperty(
+        name='Hours',
+        description="Number of hours between checking for updates",
+        default=0,
+        min=0,
+        max=23
+    )
+    updater_intrval_minutes : bpy.props.IntProperty(
+        name='Minutes',
+        description="Number of minutes between checking for updates",
+        default=0,
+        min=0,
+        max=59
+    )
+
+
+    def draw(self, context):
+        addon_updater_ops.update_settings_ui(self, context)
+
+
+classes = (BTOOLS_PT_building_tools, BTOOLS_PT_material_tools, BTOOLS_PT_PrefPanel)
 
 register_ui, unregister_ui = bpy.utils.register_classes_factory(classes)
 
 def register():
+    addon_updater_ops.register(bl_info)
+
     register_building()
     register_ui()
 
 
 def unregister():
+    addon_updater_ops.unregister()
+
     unregister_building()
-    register_ui()
+    unregister_ui()
 
 if __name__ == "__main__":
     import os
