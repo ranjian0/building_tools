c49e7a3f82cd1af609aa2624e4f81438c47ef119
ranjian0 <karanjaichungwa@gmail.com>
2021-12-01 22:05:47 +0300

    move roof build to ops

--- btools/building/roof/roof_ops.py
+++ btools/building/roof/roof_ops.py
@@ -1,6 +1,14 @@
 import bpy
+import bmesh
 
-from .roof import Roof
+from ...utils import crash_safe, get_edit_mesh
+from ..facemap import (
+    FaceMap,
+    add_facemap_for_groups,
+    verify_facemaps_for_object
+)
+
+from .roof_types import create_roof
 from .roof_props import RoofProperty
 
 
@@ -18,7 +26,41 @@ class BTOOLS_OT_add_roof(bpy.types.Operator):
         return context.object is not None and context.mode == "EDIT_MESH"
 
     def execute(self, context):
-        return Roof.build(context, self.props)
+        return build(context, self.props)
 
     def draw(self, context):
         self.props.draw(context, self.layout)
+
+
+@crash_safe
+def build(context, props):
+    verify_facemaps_for_object(context.object)
+    me = get_edit_mesh()
+    bm = bmesh.from_edit_mesh(me)
+    faces = [f for f in bm.faces if f.select]
+
+    # XXX Fix normals if they are inverted(Z-)
+    for f in faces:
+        if f.normal.z < 0:
+            f.normal_flip()
+
+    if validate_roof_faces(bm):
+        add_roof_facemaps()
+        create_roof(bm, faces, props)
+        bmesh.update_edit_mesh(me, loop_triangles=True)
+        return {"FINISHED"}
+
+    bmesh.update_edit_mesh(me, loop_triangles=True)
+    return {"CANCELLED"}
+
+
+def add_roof_facemaps():
+    add_facemap_for_groups(FaceMap.ROOF)
+
+
+def validate_roof_faces(bm):
+    faces = [f for f in bm.faces if f.select]
+    if faces:
+        if all([round(f.normal.z, 1) for f in faces]):
+            return True
+    return False
