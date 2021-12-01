d629fffec61f24a1199e317f5d6e36593fb004ed
ranjian0 <karanjaichungwa@gmail.com>
2021-12-01 21:57:41 +0300

    move floor build to ops

--- btools/building/floor/floor_ops.py
+++ btools/building/floor/floor_ops.py
@@ -1,6 +1,20 @@
 import bpy
+import bmesh
 
-from .floor import Floor
+from ..facemap import (
+    FaceMap, 
+    clear_empty_facemaps,
+    add_facemap_for_groups,
+    verify_facemaps_for_object
+)
+
+from ...utils import (
+    select,
+    crash_safe,
+    get_edit_mesh,
+)
+
+from .floor_types import create_floors
 from .floor_props import FloorProperty
 
 
@@ -18,7 +32,55 @@ class BTOOLS_OT_add_floors(bpy.types.Operator):
         return context.object is not None and context.mode == "EDIT_MESH"
 
     def execute(self, context):
-        return Floor.build(context, self.props)
+        return build(context, self.props)
 
     def draw(self, context):
         self.props.draw(context, self.layout)
+
+
+@crash_safe
+def build(context, prop):
+    verify_facemaps_for_object(context.object)
+
+    me = get_edit_mesh()
+    bm = bmesh.from_edit_mesh(me)
+
+    # XXX Fix normals if they are inverted(Z-)
+    for f in bm.faces:
+        if f.normal.z < 0:
+            f.normal_flip()
+
+    if validate_floor_faces(bm):
+        add_floor_facemaps(context, prop)
+        selected_faces = [f for f in bm.faces if f.select]
+        if selected_faces:
+            create_floors(bm, selected_faces, prop)
+            select(bm.faces, False)
+        else:
+            all_faces = [f for f in bm.faces]
+            create_floors(bm, all_faces, prop)
+        bmesh.update_edit_mesh(me, loop_triangles=True)
+        return {"FINISHED"}
+
+    bmesh.update_edit_mesh(me, loop_triangles=True)
+    return {"CANCELLED"}
+
+
+def add_floor_facemaps(context, prop):
+    clear_empty_facemaps(context)
+    groups = FaceMap.WALLS, FaceMap.ROOF
+    if prop.add_slab:
+        groups += (FaceMap.SLABS,)
+    if prop.add_columns:
+        groups += (FaceMap.COLUMNS,)
+    add_facemap_for_groups(groups)
+
+
+def validate_floor_faces(bm):
+    if any([f for f in bm.faces if f.select]):
+        selection = [f for f in bm.faces if f.select]
+        if len({round(v.co.z, 4) for f in selection for v in f.verts}) == 1:
+            return True
+    elif len({round(v.co.z, 4) for v in bm.verts}) == 1:
+        return True
+    return False
