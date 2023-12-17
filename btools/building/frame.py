bb0367c7071b31e2fb70e29aeb91478331d063bd
ranjian0 <karanjaichungwa@gmail.com>
2023-12-17 18:59:22 +0300

    Update the logic for determining the frame depth faces. closes #215

--- btools/building/frame.py
+++ btools/building/frame.py
@@ -6,26 +6,53 @@ def add_frame_depth(
 ):
     """Add depth to frame"""
     if depth != 0.0:
-        n_doors = len(door_faces)
-        n_windows = len(window_faces)
-        n_arch = len(arch_faces)
         all_faces = door_faces + window_faces + arch_faces + frame_faces
+        
+        def hash_face(f):
+            face_hash = hash((
+                f.calc_perimeter(),
+                f.calc_area(),
+                f.calc_center_median().to_tuple(4),
+            ))
+            return face_hash
+    
+        def hash_face_after(f):
+            face_hash = hash((
+                f.calc_perimeter(),
+                f.calc_area(),
+                (f.calc_center_median() + normal * depth).to_tuple(4),
+            ))
+            return face_hash
+
+        
+        door_hashes = list(map(hash_face, door_faces))
+        window_hashes = list(map(hash_face, window_faces))
+        arch_hashes = list(map(hash_face, arch_faces))
+        frame_hashes = list(map(hash_face, frame_faces))
+
         all_faces, surrounding_faces = extrude_face_region(
             bm, list(set(all_faces)), -depth, normal
         )
+        
+        new_door_faces = [f for f in all_faces if hash_face_after(f) in door_hashes]
+        new_window_faces = [f for f in all_faces if hash_face_after(f) in window_hashes]
+        new_arch_faces = [f for f in all_faces if hash_face_after(f) in arch_hashes]
+        new_frame_faces = [f for f in all_faces if hash_face_after(f) in frame_hashes]
+        
+     
         if depth < 0.0:
             return (
-                all_faces[:n_doors],
-                all_faces[n_doors : n_doors + n_windows],
-                all_faces[n_doors + n_windows : n_doors + n_windows + n_arch],
-                all_faces[n_doors + n_windows + n_arch :] + surrounding_faces,
+                new_door_faces,
+                new_window_faces,
+                new_arch_faces,
+                new_frame_faces + surrounding_faces,
             )
         else:
             return (
-                all_faces[:n_doors],
-                all_faces[n_doors : n_doors + n_windows],
-                all_faces[n_doors + n_windows : n_doors + n_windows + n_arch],
-                all_faces[n_doors + n_windows + n_arch :],
+                new_door_faces,
+                new_window_faces,
+                new_arch_faces,
+                new_frame_faces,
             )
     else:
         return door_faces, window_faces, arch_faces, frame_faces
