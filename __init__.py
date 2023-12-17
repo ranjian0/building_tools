0610b9fd916108c69decaf596951823d574664f9
ranjian0 <karanjaichungwa@gmail.com>
2023-12-17 14:31:09 +0300

    remove addon updater

--- __init__.py
+++ __init__.py
@@ -144,50 +144,7 @@ class BTOOLS_PT_material_tools(bpy.types.Panel):
                 layout.label(text=("This facemap was corrupted by a destructive operation on the object."), icon="ERROR")
 
 
-class BTOOLS_PT_PrefPanel(bpy.types.AddonPreferences):
-    bl_idname = __name__
-
-    # addon updater preferences
-    auto_check_update : bpy.props.BoolProperty(
-        name="Auto-check for Update",
-        description="If enabled, auto-check for updates using an interval",
-        default=False,
-    )
-
-    updater_intrval_months : bpy.props.IntProperty(
-        name='Months',
-        description="Number of months between checking for updates",
-        default=0,
-        min=0
-    )
-    updater_intrval_days : bpy.props.IntProperty(
-        name='Days',
-        description="Number of days between checking for updates",
-        default=7,
-        min=0,
-    )
-    updater_intrval_hours : bpy.props.IntProperty(
-        name='Hours',
-        description="Number of hours between checking for updates",
-        default=0,
-        min=0,
-        max=23
-    )
-    updater_intrval_minutes : bpy.props.IntProperty(
-        name='Minutes',
-        description="Number of minutes between checking for updates",
-        default=0,
-        min=0,
-        max=59
-    )
-
-
-    def draw(self, context):
-        from . import addon_updater_ops
-        addon_updater_ops.update_settings_ui(self, context)
-
-
-classes = (BTOOLS_PT_building_tools, BTOOLS_PT_material_tools, BTOOLS_PT_PrefPanel)
+classes = (BTOOLS_PT_building_tools, BTOOLS_PT_material_tools)
 
 register_ui, unregister_ui = bpy.utils.register_classes_factory(classes)
 
